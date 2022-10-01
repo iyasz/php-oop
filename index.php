3c9d35c53783cfa8466fc58ee62bcfebd6bd8705
Yasz Avellia <zakamaragames@gmail.com>
2022-10-02 06:44:20 +0700

    menambah function

--- index.php
+++ index.php
@@ -3,10 +3,16 @@
 class xirpl
 {
     public $nama, $umur, $alamat;
+
+    public function sayHello()
+    {
+        return "hello world";
+    }
 }
 
 $orang1 = new xirpl();
 $orang1->nama = "iyas";
 $orang1->umur = 16;
+
 
 var_dump($orang1);
