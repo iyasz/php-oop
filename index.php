47874a018a6451b5e39b91e3d1f8ac35e5531172
Yasz Avellia <zakamaragames@gmail.com>
2022-10-03 10:38:44 +0700

    menambah oop php

--- index.php
+++ index.php
@@ -14,4 +14,4 @@ $orang1 = new xirpl();
 $orang1->nama = "iyas";
 $orang1->umur = 16;
 
-echo "Nama ku : $orang1->nama <br> Umur : $orang1->umur";
+echo "Nama ku : $orang1->nama <br> Umur : $orang1->umur ";
