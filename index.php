cd5f90a8019acc92ae6ec25f700f633342db6ab4
Yasz Avellia <zakamaragames@gmail.com>
2022-10-06 11:18:56 +0700

    menambah method

--- index.php
+++ index.php
@@ -2,18 +2,21 @@
 
 class xirpl
 {
-    public $nama, $umur, $alamat;
+    public $nama = "ILyasa", $umur = 17, $alamat = "Kahuripan Mas";
 
     public function sayHello()
     {
-        return "Hello World";
+        return "Nama ku : $this->nama, Umurku : $this->umur, Alamatku : $this->alamat  ";
     }
 }
 
 $orang1 = new xirpl();
 $orang1->nama = "iyas";
 $orang1->umur = 16;
+$orang1->alamat = "isekai";
 
 echo "Nama ku : $orang1->nama <br> Umur : $orang1->umur ";
 echo "<br>";
 echo "Say hello : " . $orang1->sayHello();
+echo "<br>";
+echo $orang1->sayHello();
