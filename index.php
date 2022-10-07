0254016d345a33d1dca9448a718220635b216064
Yasz Avellia <zakamaragames@gmail.com>
2022-10-07 22:28:06 +0700

    construct

--- index.php
+++ index.php
@@ -2,25 +2,27 @@
 
 class xirpl
 {
-    public $nama = "ILyasa", $umur = 17, $alamat = "Kahuripan Mas";
+    public $nama, $umur, $alamat;
 
     public function sayHello()
     {
         return "$this->nama, $this->umur, $this->alamat  ";
     }
+
+    public function __construct($nama, $umur, $alamat)
+    {
+        $this->nama = $nama;
+        $this->umur = $umur;
+        $this->alamat = $alamat;
+    }
 }
 
-$orang1 = new xirpl();
-$orang1->nama = "iyas";
-$orang1->umur = 16;
-$orang1->alamat = "isekai";
+$orang1 = new xirpl("iyasz", 16, "Kahuripan mas");
+$orang2 = new xirpl("ELaina", 17, "Kahuripan Mas");
 
-$orang2 = new xirpl();
-$orang2->nama = "Jajan";
-$orang2->umur = 14; 
-$orang2->alamat = "GRAND";
+var_dump($orang1);
 
-echo $orang1->sayHello();
-echo "<br>";
-echo "<br>";
-echo $orang2->sayHello();
+// echo $orang1->sayHello();
+// echo "<br>";
+// echo "<br>";
+// echo $orang2->sayHello();
