8809a24dc54f7e1f2527875136cfb665d4058e56
Yasz Avellia <zakamaragames@gmail.com>
2022-10-09 18:25:11 +0700

    menambah nilai semua

--- inheritance.php
+++ inheritance.php
@@ -1,10 +1,24 @@
-<?php 
+<?php
 
-class produk{
-    public $judul, $pengarang, $penerbit, $harga, $jmlHalaman, $wwaktuMain, $tipe;
-    
-    
-    public function getInfoLengkap(){
+class produk
+{
+    public $judul, $pembuat, $penerbit, $harga, $jmlHalaman, $wwaktuMain, $episode, $tipe;
 
+
+    public function __construct($judul, $pembuat, $penerbit, $harga, $jmlHalaman, $wwaktuMain, $episode, $tipe)
+    {
+        $this->judul = $judul;
+        $this->pembuat = $pembuat;
+        $this->penerbit = $penerbit;
+        $this->harga = $harga;
+        $this->jmlHalaman = $jmlHalaman;
+        $this->wwaktuMain = $wwaktuMain;
+        $this->episode = $episode;
+        $this->tipe = $tipe;
     }
 }
+
+$produk1 = new produk("Fairy Tail", "Kamasishi", "Studio Kyoto", 0, 0, 0, 140, "Anime");
+$produk2 = new produk("Mobile Legends", "iyasz", "Montoon", 0, 0, 120, 0, "Game");
+
+var_dump($produk1);
