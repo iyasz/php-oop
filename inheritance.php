5e6a2ff01db74150c6cc0f9b91a212a7aa629e33
Yasz Avellia <zakamaragames@gmail.com>
2022-10-11 05:05:57 +0700

    menambah anime inheritance

--- inheritance.php
+++ inheritance.php
@@ -2,10 +2,10 @@
 
 class produk
 {
-    public $judul, $pembuat, $penerbit, $harga, $jmlHalaman, $waktuMain, $episode, $tipe;
+    public $judul, $pembuat, $penerbit, $harga, $jmlHalaman, $waktuMain, $episode;
 
 
-    public function __construct($judul, $pembuat, $penerbit, $harga, $jmlHalaman, $waktuMain, $episode, $tipe)
+    public function __construct($judul, $pembuat, $penerbit, $harga, $jmlHalaman, $waktuMain, $episode)
     {
         $this->judul = $judul;
         $this->pembuat = $pembuat;
@@ -14,26 +14,35 @@ class produk
         $this->jmlHalaman = $jmlHalaman;
         $this->waktuMain = $waktuMain;
         $this->episode = $episode;
-        $this->tipe = $tipe;
     }
 
     public function cetakInfo()
     {
-        $str = "{$this->judul} | {$this->pembuat}, {$this->penerbit} ";
-        if ($this->tipe == "Anime") {
-            $str .= "~ {$this->episode} Episode - {$this->tipe} ";
-        } elseif ($this->tipe == "Game") {
-            $str .= "~ {$this->waktuMain} Days - {$this->tipe} ";
-        } elseif ($this->tipe == "Manga") {
-            $str .= "~ Rp. {$this->harga} {$this->tipe}";
-        }
+        // $str = "{$this->judul} | {$this->pembuat}, {$this->penerbit} ";
+        // if ($this->tipe == "Anime") {
+        //     $str .= "~ {$this->episode} Episode - {$this->tipe} ";
+        // } elseif ($this->tipe == "Game") {
+        //     $str .= "~ {$this->waktuMain} Days - {$this->tipe} ";
+        // } elseif ($this->tipe == "Manga") {
+        //     $str .= "~ Rp. {$this->harga} {$this->tipe}";
+        // }
+
+        // return $str;
+    }
+}
+
+class anime extends produk
+{
+    public function cetakInfo()
+    {
+        $str = "Anime : {$this->judul} | {$this->pembuat}, {$this->penerbit} ~ {$this->episode} Episode  ";
         return $str;
     }
 }
 
-$produk1 = new produk("Fairy Tail", "Kamasishi", "Studio Kyoto", 0, 0, 0, 140, "Anime");
-$produk2 = new produk("Mobile Legends", "iyasz", "Montoon", 0, 0, 120, 0, "Game");
-$produk3 = new produk("Chaisaw Man", "Suzuhime", "PT jaya abadi", number_format(50000), 250, 0, 0, "Manga");
+$produk1 = new anime("Fairy Tail", "Kamasishi", "Studio Kyoto", 0, 0, 0, 140,);
+$produk2 = new produk("Mobile Legends", "iyasz", "Montoon", 0, 0, 120, 0);
+$produk3 = new produk("Chaisaw Man", "Suzuhime", "PT jaya abadi", number_format(50000), 250, 0, 0);
 
 echo $produk1->cetakInfo();
 echo "<br>";
